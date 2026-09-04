7a7381be722182ac86a1f4348f39d14c6394e6e3
Anastasiia Dekret <anastasiiadekret@yahoo.com>
2026-09-04 07:11:43 +0200

    refactor: extract sample application data

---
+++ data/applications.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    [
+        'id' => 1,
+        'company' => 'Nordlicht Digital GmbH',
+        'position' => 'Junior PHP Developer',
+        'status' => 'applied',
+        'applied_at' => '2026-08-18',
+    ],
+    [
+        'id' => 2,
+        'company' => 'RheinCode Solutions',
+        'position' => 'Frontend Developer',
+        'status' => 'interview',
+        'applied_at' => '2026-08-21',
+    ],
+    [
+        'id' => 3,
+        'company' => 'Cobalt Systems',
+        'position' => 'Junior Full Stack Developer',
+        'status' => 'rejected',
+        'applied_at' => '2026-08-23',
+    ],
+];
